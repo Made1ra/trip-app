7796e8d280cb9a841138c10ac3021b3ac58f543b
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-02-16 21:20:08 +0200

    Create data.ts

---
+++ app/lib/data.ts
@@ -0,0 +1,27 @@
+export type Trip = {
+    id: string;
+    city: string;
+    startDate: string;
+    endDate: string;
+};
+
+export const initialTrips: Trip[] = [
+    {
+        id: 'lI-vINdPoejBZ17UW4Qus',
+        city: 'Berlin',
+        startDate: '2024-07-14',
+        endDate: '2024-07-21'
+    },
+    {
+        id: 'vxYc2_g2PkLMswHYCV4IC',
+        city: 'Tokyo',
+        startDate: '2024-07-17',
+        endDate: '2024-07-23'
+    },
+    {
+        id: 'VFEMToyp3bHFW_je-odF4',
+        city: 'Barcelona',
+        startDate: '2024-07-16',
+        endDate: '2024-07-26'
+    }
+];
