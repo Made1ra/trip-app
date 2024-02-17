2c304efc55bb7d2abbae6be73482a83356dd81d7
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-02-17 19:37:48 +0200

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,20 +1,28 @@
+export const cities = ['Berlin', 'Tokyo', 'Barcelona', 'Kyiv'];
+
 export const initialTrips = [
     {
         id: 'lI-vINdPoejBZ17UW4Qus',
         city: 'Berlin',
         startDate: '2024-07-14',
-        endDate: '2024-07-21'
+        endDate: '2024-07-21',
+        imgSrc: '/berlin.jpg',
+        selected: false
     },
     {
         id: 'vxYc2_g2PkLMswHYCV4IC',
         city: 'Tokyo',
         startDate: '2024-07-17',
-        endDate: '2024-07-23'
+        endDate: '2024-07-23',
+        imgSrc: '/tokyo.jpg',
+        selected: false
     },
     {
         id: 'VFEMToyp3bHFW_je-odF4',
         city: 'Barcelona',
         startDate: '2024-07-16',
-        endDate: '2024-07-26'
+        endDate: '2024-07-26',
+        imgSrc: '/barcelona.webp',
+        selected: false
     }
 ];
