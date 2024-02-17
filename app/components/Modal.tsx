76b2cc3b0da8ea6bd7475c3ea11e75d49c35968a
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-02-17 19:38:18 +0200

    Update Modal.tsx

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,10 @@
+'use client';
+
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Trip } from '@/app/lib/definitions';
+import { cities } from '@/app/lib/data';
+import { getImageByCity } from '@/app/lib/actions';
 
 type ModalProps = {
     isOpen: boolean;
@@ -8,14 +13,44 @@ type ModalProps = {
 };
 
 export default function Modal({ isOpen, onClose, addTrip }: ModalProps) {
+    const [city, setCity] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    const minDate = new Date().toISOString().split('T')[0];
+    const maxDate = new Date(new Date().getTime() + 14 * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+
+    function handleChangeCity(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCity(event.target.value);
+    }
+
+    function handleChangeStartDate(event: React.ChangeEvent<HTMLInputElement>) {
+        setStartDate(event.target.value);
+    }
+
+    function handleChangeEndDate(event: React.ChangeEvent<HTMLInputElement>) {
+        setEndDate(event.target.value);
+    }
+
     function handleSave() {
-        addTrip({
-            id: nanoid(),
-            city: '',
-            startDate: '',
-            endDate: ''
-        });
-        onClose();
+        getImageByCity(city)
+            .then((data) => {
+                addTrip({
+                    id: nanoid(),
+                    city: city,
+                    startDate: startDate,
+                    endDate: endDate,
+                    imgSrc: data.results[0].urls.regular,
+                    selected: false
+                });
+                setCity('');
+                setStartDate('');
+                setEndDate('');
+                onClose();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     if (!isOpen) {
@@ -23,14 +58,17 @@ export default function Modal({ isOpen, onClose, addTrip }: ModalProps) {
     }
 
     return (
-        <div className="flex flex-col self-center w-max h-max bg-white border shadow fixed z-10">
+        <form
+            action={handleSave}
+            className="flex flex-col w-max h-max bg-white border shadow fixed z-10"
+        >
             <div className="flex flex-row">
                 <h1 className="text-lg font-bold m-8">
                     Create trip
                 </h1>
                 <button
                     onClick={onClose}
-                    className="ml-auto m-8"
+                    className="ml-auto m-8 hover:outline-gray-500"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -49,24 +87,25 @@ export default function Modal({ isOpen, onClose, addTrip }: ModalProps) {
                     <span className="text-red-400">*</span> City
                     <br />
                     <select
-                        defaultValue=""
+                        value={city}
+                        onChange={handleChangeCity}
+                        required
                         className="border shadow my-2 p-2 w-96 disabled:text-gray-300"
                     >
-                        <option value="" disabled>
+                        <option
+                            value=""
+                            disabled
+                        >
                             Please select a city
                         </option>
-                        <option value="berlin">
-                            Berlin
-                        </option>
-                        <option value="tokyo">
-                            Tokyo
-                        </option>
-                        <option value="barcelona">
-                            Barcelona
-                        </option>
-                        <option value="kyiv">
-                            Kyiv
-                        </option>
+                        {cities.map((city) => (
+                            <option
+                                key={city}
+                                value={city}
+                            >
+                                {city}
+                            </option>
+                        ))}
                     </select>
                 </label>
                 <label>
@@ -75,6 +114,11 @@ export default function Modal({ isOpen, onClose, addTrip }: ModalProps) {
                     <input
                         type="date"
                         placeholder="Select date"
+                        value={startDate}
+                        onChange={handleChangeStartDate}
+                        required
+                        min={minDate}
+                        max={maxDate}
                         className="my-2 border shadow p-2 w-96"
                     />
                 </label>
@@ -84,6 +128,11 @@ export default function Modal({ isOpen, onClose, addTrip }: ModalProps) {
                     <input
                         type="date"
                         placeholder="Select date"
+                        value={endDate}
+                        onChange={handleChangeEndDate}
+                        required
+                        min={minDate}
+                        max={maxDate}
                         className="my-2 border shadow p-2 w-96"
                     />
                 </label>
@@ -91,17 +140,21 @@ export default function Modal({ isOpen, onClose, addTrip }: ModalProps) {
             <div className="flex items-end justify-end ml-auto mr-4">
                 <button
                     onClick={onClose}
-                    className="rounded text-lg my-4 px-4 py-2 border shadow"
+                    className="rounded text-lg my-4 px-4 py-2 border shadow
+                    hover:border-blue-600
+                    active:border-blue-700"
                 >
                     Cancel
                 </button>
                 <button
-                    onClick={handleSave}
-                    className="rounded bg-blue-500 text-lg text-white mx-2 my-4 px-4 py-2 border shadow"
+                    type="submit"
+                    className="rounded bg-blue-500 text-lg text-white mx-2 my-4 px-4 py-2 border shadow
+                    hover:bg-blue-600
+                    active:bg-blue-700"
                 >
                     Save
                 </button>
             </div>
-        </div>
+        </form>
     );
 }
