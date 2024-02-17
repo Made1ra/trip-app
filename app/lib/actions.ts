ebb88cb47a0d29d54394fcce07a39bd7068be297
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-02-17 19:38:06 +0200

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,15 +1,36 @@
-async function getForecast(city: string, date1: string, date2: string) {
-    const API_KEY = process.env.API_KEY;
+'use server';
+
+export async function getForecast(city: string, date1: string, date2: string) {
+    const API_KEY = process.env.API_KEY || '';
+    const headers = new Headers();
+    headers.append('Authorization', API_KEY);
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/
-    timeline/${city}/${date1}/${date2}?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`);
+    timeline/${city}/${date1}/${date2}?unitGroup=metric&include=days&contentType=json`, {
+        headers: headers
+    });
 
     return await response.json();
 }
 
-async function getTodaysWeather(city: string) {
-    const API_KEY = process.env.API_KEY;
+export async function getTodaysWeather(city: string) {
+    const API_KEY = process.env.API_KEY || '';
+    const headers = new Headers();
+    headers.append('Authorization', API_KEY);
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/
-    timeline/${city}/today?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`);
+    timeline/${city}/today?unitGroup=metric&include=days&contentType=json`, {
+        headers: headers
+    });
+
+    return await response.json();
+}
+
+export async function getImageByCity(city: string) {
+    const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
+    const headers = new Headers();
+    headers.append('Authorization', `Client-ID ${UNSPLASH_API_KEY}`);
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${city}`, {
+        headers: headers
+    });
 
     return await response.json();
 }
