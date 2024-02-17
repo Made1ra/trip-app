dc53af682e8601bfcb4151f4cfce06b83e945b64
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-02-17 21:27:25 +0200

    Update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,9 +1,11 @@
 export function formatDate(date: string) {
     const parts = date.split('-');
+
     return `${parts[2]}.${parts[1]}.${parts[0]}`;
 }
 
 export function getDayOfTheWeekByDate(date: string) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
     return days[new Date(date).getDay()];
 }
