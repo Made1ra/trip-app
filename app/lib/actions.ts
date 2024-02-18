9d112f0c027fa707d19284f7a4ee4c5abe63ba6e
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-02-18 18:53:14 +0200

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,17 +2,15 @@
 
 export async function getForecast(city: string, date1: string, date2: string) {
     const API_KEY = process.env.API_KEY;
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/
-    ${city}/${date1}/${date2}?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${date1}/${date2}?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`);
 
     return await response.json();
 }
 
 export async function getTodaysWeather(city: string) {
     const API_KEY = process.env.API_KEY;
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/
-    ${city}/today?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`);
-    
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`);
+
     return await response.json();
 }
 
